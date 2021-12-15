dd9f81ecd3013712562a22916f4a2846b4e4d98f
liushuai <245691516@qq.com>
2021-12-15 15:44:40 +0800

    add dist

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: liushuai
+ * @Date: 2021-12-15 15:34:30
+ * @LastEditors: liushuai
+ * @LastEditTime: 2021-12-15 15:38:25
+ * @Description: file content
+ * @FilePath: \lss.github.io\tiezhu\src\App.vue
+-->
+<script setup>
+
+</script>
+
+<template>
+	<h1>让俺来试一试</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
